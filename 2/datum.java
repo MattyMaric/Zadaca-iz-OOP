e9ae34a03ecccadb73b71d8b7fe44adaa9b794ed
Mateo Maric <mateomaric2001@outlook.com>
2021-11-29 22:03:58 +0100

    Added spacing for if statements

--- 2/datum.java
+++ 2/datum.java
@@ -4,7 +4,7 @@ public class datum {
 
     static public void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        
+
         String mjesecText = null;
         int[] DaniUMjesecima = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
@@ -15,7 +15,7 @@ public class datum {
         System.out.print("Upisite godinu: ");
         int godina = sc.nextInt();
         sc.close();
-        
+
         switch (mjesec) {
             case 1:
                 mjesecText = "Sijecanj";
@@ -58,43 +58,54 @@ public class datum {
         if (dan > DaniUMjesecima[0] && mjesec == 1) {
             System.out.println("Sijecanj nema " + dan + " dana, nego " + DaniUMjesecima[0] + " dan");
         }
-         else if (dan > DaniUMjesecima[1] && mjesec == 2) {
+
+        else if (dan > DaniUMjesecima[1] && mjesec == 2) {
             System.out.println("Veljaca nema " + dan + " dana, nego " + DaniUMjesecima[1] + " dana");
-        } 
+        }
+
         else if (dan > DaniUMjesecima[2] && mjesec == 3) {
             System.out.println("Ozujak nema " + dan + " dana, nego " + DaniUMjesecima[2] + " dan");
         }
-         else if (dan > DaniUMjesecima[3] && mjesec == 4) {
+
+        else if (dan > DaniUMjesecima[3] && mjesec == 4) {
             System.out.println("Travanj nema " + dan + " dana, nego " + DaniUMjesecima[3] + " dana");
         }
-         else if (dan > DaniUMjesecima[4] && mjesec == 5) {
+
+        else if (dan > DaniUMjesecima[4] && mjesec == 5) {
             System.out.println("Svibanj nema " + dan + " dana, nego " + DaniUMjesecima[4] + " dan");
         }
-         else if (dan > DaniUMjesecima[5] && mjesec == 6) {
+
+        else if (dan > DaniUMjesecima[5] && mjesec == 6) {
             System.out.println("Lipanj nema " + dan + " dana, nego " + DaniUMjesecima[5] + " dana");
         }
+
         else if (dan > DaniUMjesecima[6] && mjesec == 7) {
             System.out.println("Srpanj nema " + dan + " dana, nego " + DaniUMjesecima[6] + " dan");
         }
+
         else if (dan > DaniUMjesecima[7] && mjesec == 8) {
             System.out.println("Kolovoz nema " + dan + " dana, nego " + DaniUMjesecima[7] + " dan");
         }
-         else if (dan > DaniUMjesecima[8] && mjesec == 9) {
+
+        else if (dan > DaniUMjesecima[8] && mjesec == 9) {
             System.out.println("Rujan nema " + dan + " dana, nego " + DaniUMjesecima[8] + " dana");
         }
-         else if (dan > DaniUMjesecima[9] && mjesec == 10) {
+
+        else if (dan > DaniUMjesecima[9] && mjesec == 10) {
             System.out.println("Listopad nema " + dan + " dana, nego " + DaniUMjesecima[9] + " dan");
         }
-         else if (dan > DaniUMjesecima[10] && mjesec == 11) {
+
+        else if (dan > DaniUMjesecima[10] && mjesec == 11) {
             System.out.println("Studeni nema " + dan + " dana, nego " + DaniUMjesecima[10] + " dana");
         }
-         else if (dan > DaniUMjesecima[11] && mjesec == 12) {
+
+        else if (dan > DaniUMjesecima[11] && mjesec == 12) {
             System.out.println("Prosinac nema " + dan + " dana, nego " + DaniUMjesecima[11] + " dan");
-        }else{
-            System.out.println("Upisali ste datum: " + dan + "."  + mjesecText  + "." + godina + ".");
+        } else
+
+        {
+            System.out.println("Upisali ste datum: " + dan + "." + mjesecText + "." + godina + ".");
         }
 
-
-        
     }
 }
